7c0c79b81e563e5dbac50650899db2cf5a2c2fa3
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-07-21 10:06:27 -0300

    feat: initialize project structure
    Set up the initial project structure for the application
    This is the first commit of the project, which includes basic directory layout, configuration files, and essential dependencies

---
+++ spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/SpringMvcThymeleafCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.jmfsoft.springmvccustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleafCustomerApplication.class, args);
+    }
+
+}
---
+++ spring-mvc-thymeleaf-customer/src/test/java/com/jmfsoft/springmvccustomer/SpringMvcThymeleafCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmfsoft.springmvccustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
