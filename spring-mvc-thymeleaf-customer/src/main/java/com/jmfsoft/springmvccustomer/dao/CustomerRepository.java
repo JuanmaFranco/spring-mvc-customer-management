94c7139d3a597bf857f8d0f4663dc03e2e617dc6
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-07-21 10:31:31 -0300

    feat: Implement CustomerServiceImpl with @Service annotation
    Create the CustomerServiceImpl class that implements the CustomerService interface
    The class contains the necessary methods defined in the interface
    The CustomerServiceImpl class has been annotated with @Service to mark it as a service component
    The CustomerService interface has been successfully injected into the class using constructor-based autowiring

--- spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/dao/CustomerRepository.java
+++ spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/dao/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.jmfsoft.springmvccustomer.dao;
 
 import com.jmfsoft.springmvccustomer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
---
+++ spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/service/CustomerServiceImpl.java
@@ -0,0 +1,44 @@
+package com.jmfsoft.springmvccustomer.service;
+
+import com.jmfsoft.springmvccustomer.dao.CustomerRepository;
+import com.jmfsoft.springmvccustomer.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(int id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if (customer.isEmpty()) {
+            throw new RuntimeException("Customer with id: " + id + " not found!");
+        }
+        return customer.get();
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        customerRepository.deleteById(id);
+    }
+}
