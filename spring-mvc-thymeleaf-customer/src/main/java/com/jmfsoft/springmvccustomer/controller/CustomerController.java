563a3066ff85e5f7c0d720cd090b1fa166a2e6f3
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-07-27 20:57:56 -0300

    feat: implement customer saving method with @PostMapping and add sorting in CustomerRepository
    Add the customer saving method annotated with @PostMapping to handle the addition of a new customer
    The method has been tested and verified to add customers correctly
    Additionally, introduce a method in the CustomerRepository that sorts customers by LastName in ascending order, providing an organized listing of customers based on their last names

--- spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/controller/CustomerController.java
+++ spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -33,6 +35,13 @@ public class CustomerController {
         Customer customer = new Customer();
         model.addAttribute("customer", customer);
         return "customers/customer-form";
+    }
+
+
+    @PostMapping("/save")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customers/list";
     }
 
 
--- spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/dao/CustomerRepository.java
+++ spring-mvc-thymeleaf-customer/src/main/java/com/jmfsoft/springmvccustomer/dao/CustomerRepository.java
@@ -4,7 +4,11 @@ import com.jmfsoft.springmvccustomer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    List<Customer> findAllByOrderByLastNameAsc();
 
 }
